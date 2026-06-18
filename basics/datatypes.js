9cc47083dd09d1b2651a9ed6024f184bbfb8b17a
nice1480 <drkiranarya719@gmail.com>
2026-06-18 05:32:45 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using node js not browser
+
+console.log(3 + 3)// code readability should be high
+
+console.log("hitesh")
+
+let name = ("ishita")// it is a string as it uses world
+let age = 18 // it a number
+let isloggedin = false // boolean 
+let state;
+
+// number = 2 to the power 53
+// bight
+// string = ""
+// boolean = true/false
+// null= standlone value
+// undefined = 
+// symbol= unique
+
+
+// object
+
+console.log(typeof "null");//object
+console.log(typeof undefined);// undefined
---
+++ basics/variables.js
@@ -0,0 +1,22 @@
+const accountId = 1445562
+let accountEmail = "ishitaarya@1480.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+//accountId = 2//not allowed
+
+
+accountEmail = "gygjgh.com"
+accointPassword = "43454563"
+accountCity = "bangluru"
+
+console.log(accountId);
+
+
+/*
+prefer not to use var
+because of issue in blovk scope and functional scope
+*/
+
+console.table({accountId,accountEmail,accountPassword,accountCity,accountState})
+
