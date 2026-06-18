230b1c8779dde00953406a8e05a3e6a12e5bf4b8
nice1480 <drkiranarya719@gmail.com>
2026-06-18 06:19:14 +0000

    conversion of datatype

---
+++ basics/conversionandoperations.js
@@ -0,0 +1,28 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(valueInNumber);
+
+
+// "33" = 33
+//"33abs" = NaN
+// true= 1; false=0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1= true;0=false
+// ""=false 
+"ishita"=true
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);// 33
+console.log(typeof StringNumber);//string
+
