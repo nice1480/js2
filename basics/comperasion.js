9d4298613d79c2c586f6d9cf1cde8fc20e210144
nice1480 <drkiranarya719@gmail.com>
2026-06-22 10:34:00 +0000

    revision

---
+++ basics/comperasion.js
@@ -0,0 +1,24 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2 < 1);
+//console.log(2 == 1);
+//console.log(2 != 1);
+
+//console.log("2" >1);
+//console.log("02" > 1);
+
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >= 0);
+//compersaions like 10,11,12,4.15.16 prefer not t0 use it may lead to confusion
+//console.log(undefined == 0);
+//console.log(undefined > 0);
+//console.log(undefined < 0);
+
+// === checks strictly means check data type also
+
+//console.log("2" === 2);
+
+
+
+
---
+++ basics/datatypes.summary.js
@@ -0,0 +1,6 @@
+// primitive
+
+// 7 types : string,number,boolearn,null,undefined,symbol,BigInt
+// symbol is used to make value unique
+//BigInt though all the values are cover in number but if some sone big value or scentific valuse is there we can use it in bigint
+// string is like a wprd or number in " " which is specified
