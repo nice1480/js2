89ff93d7c41c3987769a59380ccdf4541d1e26d4
nice1480 <drkiranarya719@gmail.com>
2026-06-23 10:08:11 +0000

    hghnh

--- basics/datatypes.summary.js
+++ basics/datatypes.summary.js
@@ -1,6 +1,29 @@
-// primitive
+// primitive 
+// it can not be alocate in memory 
 
 // 7 types : string,number,boolearn,null,undefined,symbol,BigInt
 // symbol is used to make value unique
+// eg- const id = symbol('123')
+//     const anotherid = symbol('123')
+// const.log(id === anotherid);  answer false
 //BigInt though all the values are cover in number but if some sone big value or scentific valuse is there we can use it in bigint
-// string is like a wprd or number in " " which is specified
+// eg - const bignumber = 35634363453656n ( n in last)
+// string is like a word or number in " " which is specified
+// number is used for numers , when a number is kept in "" it becomes string
+// boolean is answer in true or false 
+//null is nothing nothiong means nothing nor 0 nor anything
+// undefined is vvariable is given but the value of variable is not given
+
+// js is a dynamic lang
+
+
+
+// referance(non premitive)
+// it can be alocate in memory directly 
+
+// array,objects,functions
+// const heros = ["shaktiman","nagraj"] array
+// {
+//    name:"ishita",
+//    age: 22,
+// }
