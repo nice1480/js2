64f477ad803c1a4d9ab3088c355683061813132b
nice1480 <drkiranarya719@gmail.com>
2026-06-24 14:51:15 +0000

    nice

--- basics/strings.js
+++ basics/strings.js
@@ -1,4 +1,11 @@
 const name= "ishita"
-const repocount= 50
+const repocount= 2
 
-console.log(name+ repocount+"value");
+//console.log(name+ repocount+"value");
+
+console.log (`hello my name is ${name}and my repocount is ${repocount}`);
+
+const gameName  = new String("ishitahc")
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
